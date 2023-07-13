28c1f60f597dac52ff1705a738289497ac2a4501
Zufaruu <zufarbagas28@gmail.com>
2023-07-13 16:03:09 +0700

    Adding sine14bit, adc_dac, and filter

--- adc_dac_top.v
+++ adc_dac_top.v
@@ -1,46 +1,31 @@
-module adc_dac_top(
-input clk,
-output [13:0] DAC_A,
-output [13:0] DAC_B,
-output mode,
-output dac_clk_A,
-output dac_clk_B,
-output dac_wrt_A,
-output dac_wrt_B
 
+module adc_dac_top(
+	input clk,
+	input [13:0] ADC_A,
+	input   reset,
+	output [13:0] DAC_A,
+	output mode,
+	output dac_clk_A,
+	output adc_clk_A,
+	output dac_wrt_A,
+	output adc_en_A
 );
 
-reg [13:0] address;
- 
-always @(posedge clk) 
-	begin
-		address = address + 16;
-	end
+	reg [13:0] address;
+	 
+	always @(posedge clk or posedge reset) 
+		begin
+			address = ADC_A;
+		end
+	
+	assign DAC_A = address;
+	assign mode = 1;
+	assign dac_clk_A = clk;
+	assign adc_clk_A = clk;
+	assign dac_wrt_A = clk;
+	assign adc_en_A = 0;
 
-
-sine sine1 ( 
-		.address(address), 	
-		.clock(clk),	
-		.q(DAC_A)	
-	); 
-
-
-sine sine2 ( 
-		.address(address), 	
-		.clock(clk),	
-		.q(DAC_B)	
-	);
-
-assign mode = 1;
-assign dac_clk_A = clk;
-assign dac_clk_B = clk;
-assign dac_wrt_A = clk;
-assign dac_wrt_B = clk;
-
-
-
- 
- endmodule
+	endmodule
 
 
 
